3fe7244b1d0316043fa1676e9283b64451e091d0
robertjallen <bobbidigi34@gmail.com>
2020-05-10 19:35:26 -0400

    add nav styles

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import {ProtectedRoute} from './utils/ProtectedRoute'
+import {Link} from 'react-router-dom'
+import {getToken} from './utils/api'
 
 function App() {
+
+  const token = getToken()
+
   return (
     <div className="App">
-     kjhlkjhkj
+     <nav>
+      <Link to="/">Home</Link>
+			{!token && <Link to="/signin">Sign In</Link>}
+			{token && <Link to="/account">My Account</Link>}
+			{token && <Link to="/logout">Log Out</Link>}
+     </nav>
     </div>
   );
 }
