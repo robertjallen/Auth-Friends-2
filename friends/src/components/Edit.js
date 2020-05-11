403996958bb05f07687321ea41caa38d1723e26e
robertjallen <bobbidigi34@gmail.com>
2020-05-11 00:55:27 -0400

    add edit

--- friends/src/components/Edit.js
+++ friends/src/components/Edit.js
@@ -1,14 +1,34 @@
 import React, {useState} from 'react'
+import api from '../utils/api'
 
 export default function Edit(props) {
-  const [data, setData] = useState({
-		name: props.history.location.state.friend.name
-    })
 
-    console.log(props)
-    let handleSubmit
-    let error
-    let handleChange
+  const [error, setError] = useState()
+  const [data, setData] = useState(props.history.location.state.friend)
+
+    console.log("data",data)
+    const handleChange = (event) => {
+      setData({
+        ...data,
+        ...data.name,
+        [event.target.name]: event.target.value,
+      })
+      }
+    
+      const handleSubmit = (event) => {
+        event.preventDefault()
+        api()
+            .put(`/api/friends/${data.id}`, data)
+			.then(result => {
+				console.log("add result", result)
+                props.history.push('/account')
+			})
+			.catch(err => {
+				console.log(err)
+				setError(err)
+			})
+    }
+
 
 
     return (
